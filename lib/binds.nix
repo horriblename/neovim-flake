cdda400905550e13b667b2c4ebe510a4f6f674c3
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2024-08-03 18:41:41 +0200

    lib: add mkLznBinding

--- lib/binds.nix
+++ lib/binds.nix
@@ -65,7 +65,39 @@
   mkSetLuaBinding = mode: binding: action:
     mkLuaBinding mode binding.value action binding.description;
 
-  pushDownDefault = attr: mapAttrs (_: mkDefault) attr;
-in {
-  inherit mkLuaBinding mkExprBinding mkBinding mkMappingOption addDescriptionsToMappings mkSetBinding mkSetExprBinding mkSetLuaBinding pushDownDefault;
-}
+    # Utility function that takes two attrsets:
+    # { someKey = "some_value" } and
+    # { someKey = { description = "Some Description"; }; }
+    # and merges them into
+    # { someKey = { value = "some_value"; description = "Some Description"; }; }
+    addDescriptionsToMappings = actualMappings: mappingDefinitions:
+      mapAttrs (name: value: let
+        isNested = isAttrs value;
+        returnedValue =
+          if isNested
+          then addDescriptionsToMappings actualMappings."${name}" mappingDefinitions."${name}"
+          else {
+            inherit value;
+            inherit (mappingDefinitions."${name}") description;
+          };
+      in
+        returnedValue)
+      actualMappings;
+
+    mkSetBinding = binding: action:
+      mkBinding binding.value action binding.description;
+
+    mkSetExprBinding = binding: action:
+      mkExprBinding binding.value action binding.description;
+
+    mkSetLuaBinding = binding: action:
+      mkLuaBinding binding.value action binding.description;
+
+    pushDownDefault = attr: mapAttrs (_: mkDefault) attr;
+
+    mkLznBinding = mode: lhs: rhs: desc: {
+      inherit mode lhs rhs desc;
+    };
+  };
+in
+  binds
